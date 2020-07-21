9b983569d3baeb0271db5ff226eccd6aca7bfed8
kostasfnrs <68249946+kostasfnrs@users.noreply.github.com>
2020-07-21 18:42:27 +0300

    Update webhook_verify.js

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -3,13 +3,7 @@ const processMessage = require('../processes/messages');
 
 module.exports = function(app, chalk){
   app.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
-       console.log('webhook verified');
-       res.status(200).send(req.query['hub.challenge']);
-    } else {
-        console.error('verification failed. Token mismatch.');
-        res.sendStatus(403);
-     }
+    res.status(200).send(req.query['hub.challenge']);
   });
   
   app.post('/webhook', function(req, res) {
